a70fe8e247c6982814bd7f358f80fe2399fd1128
erbihari <58420661+erbihari@users.noreply.github.com>
2019-12-17 17:15:54 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>OSMS</title>
+</head>
+
+<body>
+  <!--Start Contact Us-->
+  <div class="container" id="Contact">
+    <!--Start Contact Us Container-->
+    <h2 class="text-center mb-4">Contact US</h2> <!-- Contact Us Heading -->
+    <div class="row">
+
+      <!--Start Contact Us Row-->
+      <?php include('contactform.php'); ?>
+      <!-- End Contact Us 1st Column -->
+
+    </div> <!-- End Contact Us Row-->
+  </div> <!-- End Contact Us Container-->
+  <!-- End Contact Us -->
+</body>
+
+</html>
